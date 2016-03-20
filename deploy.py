b64fed92e60896e7023b69f1167c08291c23a3e4
Juhwan Yun <btakeya.dev@gmail.com>
2016-03-20 16:24:56 +0900

    Some changes on deployment
    - remove port from metadata
    - duplexing for deploy (mode #0 & #1)

--- deploy.py
+++ deploy.py
@@ -2,10 +2,17 @@
 
 import os
 import json
+import subprocess
+import shutil
 
-project_root = os.path.realpath('')
-metafile = project_root + '/deploy.info'
+PROJECT_ROOT = os.path.realpath('')
+METAFILE = PROJECT_ROOT + '/deploy.info'
 BASE_PORT = 9000
+PROJECT_NAME = 'aroundplay'
+PROJECT_VERSION = '1.0-SNAPSHOT'
+PROJECT_TARGET = 'package/%s-%s' % (PROJECT_NAME, PROJECT_VERSION)
+PROJECT_PACKAGE = '%s.zip' % (PROJECT_TARGET)
+DEPLOY_DIR = 'deploy/deploy'
 
 class BoltMetadata(object):
     def __init__(self):
@@ -19,26 +26,36 @@ class BoltMetadata(object):
         meta_json = json.loads(meta)
         self.mode = meta_json['mode']
         self.pid = meta_json['pid']
-        self.port = meta_json['port']
         meta_file.close()
 
     def save_file(self, metafile):
         meta_file = open(metafile, 'w')
-        meta_dict = {'mode': self.mode, 'pid': self.pid, 'port': self.port}
+        meta_dict = {'mode': self.mode, 'pid': self.pid}
         meta_str = json.dumps(meta_dict)
         meta_file.write(meta_str)
         meta_file.close()
+
+    def prepare_server(self):
+        self.unpack()
+
+    def unpack(self):
+        DIST_DIR = '%s-%d' % (DEPLOY_DIR, self.mode)
+        if os.path.isdir(DIST_DIR):
+            shutil.rmtree(DIST_DIR)
+        subprocess.call(['unzip', PROJECT_PACKAGE, '-d', DIST_DIR])
 
     def run_server(self):
         return 0
 
     def toString(self):
-        return 'BoltMetadata(mode: %d, pid: %d, port: %d' % (self.mode, self.pid, self.port)
+        return 'BoltMetadata(mode: %d, pid: %d)' % (self.mode, self.pid)
 
 def run():
     meta = BoltMetadata()
-    meta.load_file(metafile)
-    meta.save_file(metafile)
+    meta.load_file(METAFILE)
+    meta.mode = 1 - meta.mode
+    meta.prepare_server()
+    meta.save_file(METAFILE)
 
 if __name__ == '__main__':
     run()
